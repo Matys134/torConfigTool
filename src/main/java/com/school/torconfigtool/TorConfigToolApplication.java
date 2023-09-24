bf1dbe3a45490ae02a4535f85fa0972fd330de7f
Matys <koubamates4@gmail.com>
2023-09-24 11:18:25 +0200

    Initial commit

---
+++ src/main/java/com/school/torconfigtool/TorConfigToolApplication.java
@@ -0,0 +1,13 @@
+package com.school.torconfigtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorConfigToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorConfigToolApplication.class, args);
+	}
+
+}
