5fa21a0c3230c61e4aa567486766df0734e95c4b
Matys <koubamates4@gmail.com>
2023-10-09 12:02:40 +0200

    update

--- src/main/java/com/school/torconfigtool/controllers/RelayController.java
+++ src/main/java/com/school/torconfigtool/controllers/RelayController.java
@@ -89,57 +89,25 @@ public class RelayController {
             String relayContact
     ) {
         try {
-            // Read the existing Tor Relay configuration file (torrc)
+            // Create a temporary file to hold the updated configuration
+            File tempFile = new File("torrc/local-torrc.temp"); // Temporary file path
+            BufferedWriter tempWriter = new BufferedWriter(new FileWriter(tempFile));
+
+            // Write the updated configuration to the temporary file
+            tempWriter.write("Nickname " + relayNickname + "\n");
+            tempWriter.write("BandwidthRate " + relayBandwidth + " KBytes\n");
+            tempWriter.write("ORPort " + relayPort + "\n");
+            tempWriter.write("ContactInfo " + relayContact + "\n");
+
+            // Close the temporary file writer
+            tempWriter.close();
+
+            // Replace the existing configuration file with the temporary file
             File torrcFile = new File("torrc/local-torrc"); // Adjust the path to your torrc file
-            BufferedReader reader = new BufferedReader(new FileReader(torrcFile));
-            String line;
-            StringBuilder newTorrcContent = new StringBuilder();
-
-            // Flags to check if lines exist
-            boolean nicknameExists = false;
-            boolean bandwidthRateExists = false;
-            boolean orPortExists = false;
-            boolean contactInfoExists = false;
-
-            while ((line = reader.readLine()) != null) {
-                // Modify the relevant configuration parameters based on user input
-                if (line.startsWith("Nickname ")) {
-                    newTorrcContent.append("Nickname ").append(relayNickname).append("\n");
-                    nicknameExists = true;
-                } else if (line.startsWith("BandwidthRate ")) {
-                    newTorrcContent.append("BandwidthRate ").append(relayBandwidth).append(" KBytes\n");
-                    bandwidthRateExists = true;
-                } else if (line.startsWith("ORPort ")) {
-                    newTorrcContent.append("ORPort ").append(relayPort).append("\n");
-                    orPortExists = true;
-                } else if (line.startsWith("ContactInfo ")) {
-                    newTorrcContent.append("ContactInfo ").append(relayContact).append("\n");
-                    contactInfoExists = true;
-                } else {
-                    newTorrcContent.append(line).append("\n");
-                }
+            if (torrcFile.exists()) {
+                torrcFile.delete();
             }
-
-            reader.close();
-
-            // If the lines don't exist, create and append them
-            if (!nicknameExists) {
-                newTorrcContent.append("Nickname ").append(relayNickname).append("\n");
-            }
-            if (!bandwidthRateExists) {
-                newTorrcContent.append("BandwidthRate ").append(relayBandwidth).append(" KBytes\n");
-            }
-            if (!orPortExists) {
-                newTorrcContent.append("ORPort ").append(relayPort).append("\n");
-            }
-            if (!contactInfoExists) {
-                newTorrcContent.append("ContactInfo ").append(relayContact).append("\n");
-            }
-
-            // Write the updated configuration back to the torrc file
-            BufferedWriter writer = new BufferedWriter(new FileWriter(torrcFile));
-            writer.write(newTorrcContent.toString());
-            writer.close();
+            tempFile.renameTo(torrcFile);
 
             // Return true if configuration update is successful
             return true;
@@ -149,6 +117,67 @@ public class RelayController {
             return false;
         }
     }
+
+    @PostMapping("/configure-bridge")
+    public String configureBridge(@RequestParam int bridgePort,
+                                  @RequestParam String bridgeEmail,
+                                  @RequestParam String bridgeNickname,
+                                  Model model) {
+        // Implement logic to update the bridge configuration
+        boolean configurationSuccess = updateBridgeConfiguration(
+                bridgePort,
+                bridgeEmail,
+                bridgeNickname
+        );
+
+        if (configurationSuccess) {
+            model.addAttribute("successMessage", "Bridge configuration updated successfully!");
+        } else {
+            model.addAttribute("errorMessage", "Failed to update bridge configuration.");
+        }
+
+        return "relay-config"; // Thymeleaf template name (relay-config.html)
+    }
+
+    private boolean updateBridgeConfiguration(
+            int bridgePort,
+            String bridgeEmail,
+            String bridgeNickname
+    ) {
+        try {
+            // Create a temporary file for bridge configuration
+            File tempFile = new File("torrc/local-torrc-bridge.temp"); // Temporary file path for bridge configuration
+            BufferedWriter tempWriter = new BufferedWriter(new FileWriter(tempFile));
+
+            // Write bridge configuration lines to the temporary file
+            tempWriter.write("BridgeRelay 1\n");
+            tempWriter.write("ORPort " + bridgePort + "\n");
+            tempWriter.write("ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy\n");
+            tempWriter.write("ServerTransportListenAddr obfs4 0.0.0.0:" + bridgePort + "\n");
+            tempWriter.write("ExtORPort auto\n");
+            tempWriter.write("ContactInfo " + bridgeEmail + "\n");
+            tempWriter.write("Nickname " + bridgeNickname + "\n");
+
+            // Close the temporary file writer
+            tempWriter.close();
+
+            // Replace the existing bridge configuration file with the temporary file
+            File bridgeConfigFile = new File("torrc/local-torrc-bridge"); // Adjust the path to your bridge config file
+            if (bridgeConfigFile.exists()) {
+                bridgeConfigFile.delete();
+            }
+            tempFile.renameTo(bridgeConfigFile);
+
+            // Return true if configuration update is successful
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Log and handle any exceptions that occur during the update
+            return false;
+        }
+    }
+
+
 
     private boolean restartTorRelayService() {
         try {
