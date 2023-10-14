2ed34426573b2d893c8f8ffcb1e4843b3733c9d3
Matys <koubamates4@gmail.com>
2023-10-14 21:10:28 +0200

    update

--- shellScripts/configure-relay.sh
+++ shellScripts/configure-relay.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# Get the directory of the shell script
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Get the directory of the script
+script_dir=$(dirname "$0")
 
-# Define the path to the torrc file relative to the script location
-torrc_file="$script_dir/torrc/local-torrc"
+# Define the path to the torrc file relative to the script directory
+torrc_file="$script_dir/../torrc/local-torrc"
 
-# Clear the torrc file
-> "$torrc_file"
+# Check if the torrc file exists
+if [ -f "$torrc_file" ]; then
+    # Clear the torrc file
+    > "$torrc_file"
 
-# Add the desired configuration lines to the torrc file
-cat <<EOF > "$torrc_file"
-Nickname    myNiceRelay  # Change "myNiceRelay" to something you like
-ContactInfo your@e-mail  # Write your e-mail and be aware it will be published
-ORPort      443          # You might use a different port, should you want to
-ExitRelay   0
-SocksPort   0
-EOF
+    # Add the specified lines to the torrc file
+    echo 'Nickname    myNiceRelay  # Change "myNiceRelay" to something you like' >> "$torrc_file"
+    echo 'ContactInfo your@e-mail  # Write your e-mail and be aware it will be published' >> "$torrc_file"
+    echo 'ORPort      443          # You might use a different port, should you want to' >> "$torrc_file"
+    echo 'ExitRelay   0' >> "$torrc_file"
+    echo 'SocksPort   0' >> "$torrc_file"
 
-echo "torrc/local-torrc has been updated."
+    echo "torrc file has been cleared and updated."
+else
+    echo "torrc file not found at: $torrc_file"
+fi
