5f1f925a626886997b4799e4984c9785fa25b153
Matys <koubamates4@gmail.com>
2023-10-15 15:37:56 +0200

    update

--- shellScripts/sudoers.sh
+++ shellScripts/sudoers.sh
@@ -11,25 +11,37 @@ configure_relay_script="$script_dir/$script_name"
 
 # Check if the configure-relay.sh file exists
 if [ -f "$configure_relay_script" ]; then
-    # Define the sudoers entry
-    sudoers_entry="$(whoami) ALL=(ALL) NOPASSWD: $configure_relay_script"
 
-    # Define the path to the sudoers file
-    sudoers_file="/etc/sudoers.d/torsudoers"
+    # Determine the original user who invoked sudo
+    original_user="$SUDO_USER"
 
-    # Check if the sudoers file exists, and create it if it doesn't
-    if [ ! -f "$sudoers_file" ]; then
-        touch "$sudoers_file"
-    fi
+    if [ -n "$original_user" ]; then
+        # Define the sudoers entry with the original user's username
+        sudoers_entry="$original_user ALL=(ALL) NOPASSWD: $configure_relay_script"
 
-    # Check if the sudoers file is writable
-    if [ -w "$sudoers_file" ]; then
-        # Add the sudoers entry
-        echo "$sudoers_entry" | sudo tee -a "$sudoers_file" > /dev/null
+        # Define the path to the sudoers file
+        sudoers_file="/etc/sudoers.d/torsudoers"
 
-        echo "Sudoers entry added for $configure_relay_script"
+        # Check if the sudoers file exists, and create it if it doesn't
+        if [ ! -f "$sudoers_file" ]; then
+            touch "$sudoers_file"
+        fi
+
+        # Check if the sudoers file is writable
+        if [ -w "$sudoers_file" ]; then
+
+            # Clear the sudoers file
+            > "$sudoers_file"
+
+            # Add the sudoers entry
+            echo "$sudoers_entry" | sudo tee -a "$sudoers_file" > /dev/null
+
+            echo "Sudoers entry added for $configure_relay_script, assigned to user: $original_user"
+        else
+            echo "Cannot write to $sudoers_file. Please run this script with superuser privileges."
+        fi
     else
-        echo "Cannot write to $sudoers_file. Please run this script with superuser privileges."
+        echo "Unable to determine the original user who invoked sudo."
     fi
 else
     echo "$script_name not found at: $configure_relay_script"
