b22e4a2155dc5bdd3abcb8b331bdca2d2e081bee
Matys <koubamates4@gmail.com>
2023-10-15 14:54:58 +0200

    update

--- shellScripts/sudoers.sh
+++ shellScripts/sudoers.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
 
-# Determine the full path of the script
-script_dir="$(dirname "$(readlink -f "$0")")"
+# Get the current user
+current_user=$(whoami)
 
-# Define the name of the script
-script_name="configure-relay.sh"
+# Get the directory of the script
+script_dir=$(dirname "$0")
 
-# Define the full path to the configure-relay.sh script
-configure_relay_script="$script_dir/$script_name"
+# Define the path to the configure-relay.sh script relative to the script directory
+configure_relay_script="$script_dir/configure-relay.sh"
 
-# Check if the configure-relay.sh file exists
+# Check if the configure-relay.sh script exists
 if [ -f "$configure_relay_script" ]; then
-    # Define the sudoers entry
-    sudoers_entry="$(whoami) ALL=(ALL) NOPASSWD: $configure_relay_script"
+    # Create or append to the sudoers file
+    echo "$current_user ALL=(ALL:ALL) NOPASSWD: $configure_relay_script" | sudo tee -a /etc/sudoers.d/torsudoers
 
-    # Define the path to the sudoers file
-    sudoers_file="/etc/sudoers.d/torsudoers"
-
-    # Check if the sudoers file exists, and create it if it doesn't
-    if [ ! -f "$sudoers_file" ]; then
-        touch "$sudoers_file"
-    fi
-
-    # Check if the sudoers file is writable
-    if [ -w "$sudoers_file" ]; then
-        # Add the sudoers entry
-        echo "$sudoers_entry" | sudo tee -a "$sudoers_file" > /dev/null
-
-        echo "Sudoers entry added for $configure_relay_script"
-    else
-        echo "Cannot write to $sudoers_file. Please run this script with superuser privileges."
-    fi
+    echo "User $current_user can run $configure_relay_script without a password."
 else
-    echo "$script_name not found at: $configure_relay_script"
+    echo "configure-relay.sh script not found at: $configure_relay_script"
 fi
