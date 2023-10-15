3fe347a148669a85b4606762b3526ee674468d74
Matys <koubamates4@gmail.com>
2023-10-15 15:29:28 +0200

    update

--- shellScripts/sudoers.sh
+++ shellScripts/sudoers.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# Check the EUID (effective user ID) to see who switched to su
-if [ $EUID -eq 0 ]; then
-    # You are currently the root user
-    if [ -n "$SUDO_USER" ]; then
-        # The user who switched to root is in $SUDO_USER
-        user_to_grant_sudo="$SUDO_USER"
+# Determine the full path of the script
+script_dir="$(dirname "$(readlink -f "$0")")"
 
-        # Define the sudoers entry for the specific user
-        sudoers_entry="$user_to_grant_sudo ALL=(ALL) NOPASSWD: $configure_relay_script"
+# Define the name of the script
+script_name="configure-relay.sh"
 
-        # Define the path to the sudoers file
-        sudoers_file="/etc/sudoers.d/torsudoers"
+# Define the full path to the configure-relay.sh script
+configure_relay_script="$script_dir/$script_name"
 
-        # Check if the sudoers file exists, and create it if it doesn't
-        if [ ! -f "$sudoers_file" ]; then
-            touch "$sudoers_file"
-        fi
+# Check if the configure-relay.sh file exists
+if [ -f "$configure_relay_script" ]; then
+    # Define the sudoers entry
+    sudoers_entry="$(whoami) ALL=(ALL) NOPASSWD: $configure_relay_script"
 
-        # Check if the sudoers file is writable
-        if [ -w "$sudoers_file" ]; then
-            # Add the sudoers entry for the specific user
-            echo "$sudoers_entry" | sudo tee -a "$sudoers_file" > /dev/null
+    # Define the path to the sudoers file
+    sudoers_file="/etc/sudoers.d/torsudoers"
 
-            echo "Sudoers entry added for $user_to_grant_sudo to run $configure_relay_script without a password."
-        else
-            echo "Cannot write to $sudoers_file. Please run this script with superuser privileges."
-        fi
+    # Check if the sudoers file exists, and create it if it doesn't
+    if [ ! -f "$sudoers_file" ]; then
+        touch "$sudoers_file"
+    fi
+
+    # Check if the sudoers file is writable
+    if [ -w "$sudoers_file" ]; then
+        # Add the sudoers entry
+        echo "$sudoers_entry" | sudo tee -a "$sudoers_file" > /dev/null
+
+        echo "Sudoers entry added for $configure_relay_script"
     else
-        echo "You switched to root using 'su' without specifying a user. Please specify a user with 'su - user' and then run this script again."
+        echo "Cannot write to $sudoers_file. Please run this script with superuser privileges."
     fi
 else
-    # You are not running as root
-    echo "You are not running as root. Please switch to root using 'su' before running this script."
+    echo "$script_name not found at: $configure_relay_script"
 fi
