999f9a9bda8ee9e9fd429d247ce7df4a403ca93b
Matys <koubamates4@gmail.com>
2023-10-16 11:52:43 +0200

    update

--- src/main/java/com/school/torconfigtool/controllers/BridgeController.java
+++ src/main/java/com/school/torconfigtool/controllers/BridgeController.java
@@ -2,81 +2,51 @@ package com.school.torconfigtool.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 
 @Controller
 @RequestMapping("/bridge")
 public class BridgeController {
 
-    @RequestMapping
+    @GetMapping
     public String bridgeConfigurationForm() {
-        return "bridge-config"; // Thymeleaf template name (bridge-config.html)
+        return "relay-config"; // Thymeleaf template name (bridge-config.html)
     }
 
     @PostMapping("/configure")
     public String configureBridge(@RequestParam int bridgePort,
-                                  @RequestParam String bridgeEmail,
+                                  @RequestParam int bridgeTransportListenAddr,
+                                  @RequestParam String bridgeContact,
                                   @RequestParam String bridgeNickname,
                                   Model model) {
-        // Implement logic to update the bridge configuration
-        boolean configurationSuccess = updateBridgeConfiguration(
-                bridgePort,
-                bridgeEmail,
-                bridgeNickname
-        );
-
-        if (configurationSuccess) {
-            model.addAttribute("successMessage", "Bridge configuration updated successfully!");
-        } else {
-            model.addAttribute("errorMessage", "Failed to update bridge configuration.");
-        }
-
-        return "relay-config"; // Thymeleaf template name (relay-config.html)
-    }
-
-    private boolean updateBridgeConfiguration(
-            int bridgePort,
-            String bridgeEmail,
-            String bridgeNickname
-    ) {
         try {
-            // Create a temporary file for bridge configuration
-            File tempFile = new File("torrc/local-torrc-bridge.temp"); // Temporary file path for bridge configuration
-            BufferedWriter tempWriter = new BufferedWriter(new FileWriter(tempFile));
+            // Define the path to the shell script
+            String scriptPath = "shellScripts/configure-bridge.sh";
 
-            // Write bridge configuration lines to the temporary file
-            tempWriter.write("BridgeRelay 1\n");
-            tempWriter.write("ORPort " + bridgePort + "\n");
-            tempWriter.write("ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy\n");
-            tempWriter.write("ServerTransportListenAddr obfs4 0.0.0.0:" + bridgePort + "\n");
-            tempWriter.write("ExtORPort auto\n");
-            tempWriter.write("ContactInfo " + bridgeEmail + "\n");
-            tempWriter.write("Nickname " + bridgeNickname + "\n");
+            // Create a process builder for the script
+            ProcessBuilder processBuilder = new ProcessBuilder("bash", scriptPath,
+                    String.valueOf(bridgePort), String.valueOf(bridgeTransportListenAddr), bridgeContact, bridgeNickname);
 
-            // Close the temporary file writer
-            tempWriter.close();
+            // Start the process and wait for it to complete
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
 
-            // Replace the existing bridge configuration file with the temporary file
-            File bridgeConfigFile = new File("torrc/local-torrc-bridge"); // Adjust the path to your bridge config file
-            if (bridgeConfigFile.exists()) {
-                bridgeConfigFile.delete();
+            if (exitCode == 0) {
+                model.addAttribute("successMessage", "Tor Bridge configured and restarted successfully!");
+            } else {
+                model.addAttribute("errorMessage", "Failed to configure Tor Bridge.");
             }
-            tempFile.renameTo(bridgeConfigFile);
-
-            // Return true if configuration update is successful
-            return true;
         } catch (Exception e) {
             e.printStackTrace();
-            // Log and handle any exceptions that occur during the update
-            return false;
+            model.addAttribute("errorMessage", "Failed to run configuration script.");
         }
+
+        return "bridge-config"; // Thymeleaf template name (bridge-config.html)
     }
+
 
     private boolean restartTorRelayService() {
         try {
