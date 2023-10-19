f166beccbe752e2148c6e61205dec040bd7a6c06
Matys <koubamates4@gmail.com>
2023-10-19 11:34:15 +0200

    Revert "edit"
    This reverts commit 694524d6ef9d6c7129fed67af2439aa371726334.

--- src/main/java/com/school/torconfigtool/controllers/UpdateRelayConfigurationController.java
+++ src/main/java/com/school/torconfigtool/controllers/UpdateRelayConfigurationController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/update-relay-configuration")
@@ -17,33 +15,32 @@ public class UpdateRelayConfigurationController {
 
     @PostMapping
     public UpdateResponse updateRelayConfiguration(@RequestBody UpdateRequest request) {
-        String scriptPath = "/path/to/your/update-relay-configuration.sh"; // Adjust the path to your shell script
+        // Build the shell command to execute the update script
+        String scriptPath = "shellScripts/configure-relay.sh"; // Adjust the path to your shell script
+        String command = scriptPath + " " + request.getNickname() + " " +
+                request.getBandwidth() + " " + request.getPort() + " " + request.getContact();
 
         try {
-            ProcessBuilder processBuilder = new ProcessBuilder(scriptPath, request.getNickname(), request.getBandwidth(), request.getPort(), request.getContact());
-            processBuilder.redirectErrorStream(true);
-
-            Process process = processBuilder.start();
-            process.waitFor();
+            // Execute the shell script
+            Process process = Runtime.getRuntime().exec(command);
 
             // Capture the script's output
-            List<String> outputLines = new ArrayList<>();
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    outputLines.add(line);
-                }
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
             }
 
-            int exitCode = process.exitValue();
+            // Wait for the script to complete
+            int exitCode = process.waitFor();
 
             if (exitCode == 0) {
                 // Successful update
                 return new UpdateResponse(true, "Relay configuration updated successfully.");
             } else {
                 // Update failed
-                String outputMessage = String.join("\n", outputLines);
-                return new UpdateResponse(false, "Failed to update relay configuration. Script output: " + outputMessage);
+                return new UpdateResponse(false, "Failed to update relay configuration. Script output: " + output.toString());
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
