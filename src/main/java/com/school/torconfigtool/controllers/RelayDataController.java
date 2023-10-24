8289872f0755dbc0ef8e4c09dfb7f4d0841e0f4d
Matys <koubamates4@gmail.com>
2023-10-24 20:58:14 +0200

    test

--- src/main/java/com/school/torconfigtool/controllers/RelayDataController.java
+++ src/main/java/com/school/torconfigtool/controllers/RelayDataController.java
@@ -1,29 +1,27 @@
 package com.school.torconfigtool.controllers;
 
 import com.school.torconfigtool.models.RelayData;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class RelayDataController {
 
-    private List<RelayData> relayDataList = new ArrayList<>();
-
-    @PostMapping("/relay-data")
-    public ResponseEntity<String> receiveRelayData(@RequestBody RelayData relayData) {
-        // Store the relay data
-        relayDataList.add(relayData);
-
-        return ResponseEntity.ok("Data received successfully");
-    }
+    // Sample relay data list (replace this with your actual data source)
+    private List<RelayData> relayDataList = new ArrayList<>(); // Make sure this list is populated with data
 
     @GetMapping("/relay-data")
-    public List<RelayData> getRelayData() {
-        // Return the list of stored relay data
-        return relayDataList;
+    public String getRelayData(Model model) {
+        // Get the last relay data item (latest data)
+        RelayData latestData = relayDataList.isEmpty() ? new RelayData() : relayDataList.get(relayDataList.size() - 1);
+
+        // Add the latest data to the model
+        model.addAttribute("latestRelayData", latestData);
+
+        return "relay-data"; // Use the appropriate Thymeleaf template name
     }
 }
