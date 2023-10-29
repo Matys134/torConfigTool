6f4242aef5dd040ed77c9adf3862389f400d31f5
Matys <koubamates4@gmail.com>
2023-10-29 23:19:29 +0100

    Update - multiple graphs

--- src/main/pyScripts/__init__.py
+++ src/main/pyScripts/__init__.py
@@ -1,49 +1,64 @@
+import os
 import functools
-from stem.control import Controller
+import threading
+from stem.control import EventType, Controller
 import requests
-
-# Function to create a new listener for a relay
-def create_relay_listener(port):
-    def relay_listener(controller, event):
-        download = event.read
-        upload = event.written
-
-        data = {'download': download, 'upload': upload}
-        api_url = f'http://localhost:{port}/api/relay-data'
-        response = requests.post(api_url, json=data)
-
-        print(f"Downloaded: {event.read} bytes/s, Uploaded: {event.written} bytes/s")
-
-        if response.status_code == 200:
-            print('Data sent successfully')
-        else:
-            print('Failed to send data')
-
-    return relay_listener
+import time
 
 def main():
-    # Read control ports from torrc/guard files
+    # Define the directory containing Tor control files
+    torrc_dir = "/home/matys/IdeaProjects/torConfigTool/torrc/guard"
     control_ports = []
-    with open('torrc/guard', 'r') as file:
-        for line in file:
-            if line.startswith("ControlPort"):
-                parts = line.split()
-                if len(parts) == 2:
-                    control_ports.append(int(parts[1]))
 
-    # Create listeners for each relay
-    listeners = []
-    for port in control_ports:
-        with Controller.from_port(port=port) as controller:
-            controller.authenticate()
-            relay_listener = create_relay_listener(port)
-            controller.add_event_listener(relay_listener, EventType.BW)
-            listeners.append(controller)
+    # Iterate through the files in the directory and collect control ports
+    for filename in os.listdir(torrc_dir):
+        control_port = read_control_port(os.path.join(torrc_dir, filename))
+        if control_port:
+            control_ports.append(control_port)
 
+    # Create a thread for each relay and start monitoring
+    threads = []
+    for control_port in control_ports:
+        thread = threading.Thread(target=monitor_traffic, args=(control_port,))
+        thread.setDaemon(True)  # Set the thread as a daemon
+        threads.append(thread)
+        thread.start()
+
+    # Wait for user input to stop monitoring
+    input("Press Enter to stop monitoring")
+
+def read_control_port(file_path):
+    # Read the control port from the Tor configuration file
     try:
-        input("Press Enter to exit")
-    except KeyboardInterrupt:
-        pass  # the user hit ctrl+c
+        with open(file_path, 'r') as file:
+            for line in file:
+                if line.startswith("ControlPort"):
+                    parts = line.strip().split()
+                    if len(parts) == 2:
+                        return int(parts[1])
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"Error reading control port from {file_path}: {str(e)}")
+    return None
+
+def monitor_traffic(control_port):
+    with Controller.from_port(port=control_port) as controller:
+        controller.authenticate()
+        bw_event_handler = functools.partial(_handle_bandwidth_event, controller)
+        controller.add_event_listener(bw_event_handler, EventType.BW)
+
+        print(f"Monitoring relay on ControlPort {control_port}")
+
+        while True:
+            time.sleep(1)  # Wait for 1 second to collect data
+            # In this loop, the script continuously prints traffic per second
+
+def _handle_bandwidth_event(controller, event):
+    download = event.read
+    upload = event.written
+
+    print(f"Downloaded: {event.read} bytes/s, Uploaded: {event.written} bytes/s")
 
 if __name__ == '__main__':
     main()
