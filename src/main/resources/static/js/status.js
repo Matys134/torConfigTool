e1ec7e174293db567cfbab35f358a85b3e53852e
Matys <koubamates4@gmail.com>
2023-10-29 16:01:22 +0100

    Change status - AJAX

--- src/main/resources/static/js/status.js
+++ src/main/resources/static/js/status.js
@@ -31,20 +31,6 @@ $(document).ready(function() {
     // Update relay status on page load
     updateAllRelayStatus();
 
-    // Function to handle the "Stop" button click event
-    $(".stop-button").click(function() {
-        var nickname = $(this).data("config-nickname");
-        var statusMessageElement = $("#status-message-" + nickname);
-
-        // Change the status message to "Shutting down"
-        statusMessageElement.text("Shutting down");
-
-        // Send a POST request to stop the relay using the corresponding nickname
-        $.post("/relay-operations/stop", { relayNickname: nickname }, function(data) {
-            // Handle the response, e.g., show success or error messages
-        });
-    });
-
     // Periodically update relay status every 10 seconds (adjust the interval as needed)
-    setInterval(updateAllRelayStatus, 10000);
+    setInterval(updateAllRelayStatus, 1000);
 });
