de3a705799f6b99e5273be460e5f4e29de558cb4
Matys <koubamates4@gmail.com>
2023-10-29 10:38:16 +0100

    Shutting down text

--- src/main/resources/static/js/status.js
+++ src/main/resources/static/js/status.js
@@ -3,12 +3,19 @@ $(document).ready(function() {
     function updateRelayStatus(nickname) {
         $.get("/relay/status?relayNickname=" + nickname, function(data) {
             var statusElement = $("#status-" + nickname);
+            var statusMessageElement = $("#status-message-" + nickname);
+
             if (data === "online") {
                 statusElement.text("Online");
                 statusElement.css("color", "green");
             } else {
                 statusElement.text("Offline");
                 statusElement.css("color", "red");
+            }
+
+            // Check the status message and update accordingly
+            if (statusMessageElement.text() === "Shutting down") {
+                statusMessageElement.text("Idle");
             }
         });
     }
@@ -24,6 +31,20 @@ $(document).ready(function() {
     // Update relay status on page load
     updateAllRelayStatus();
 
+    // Function to handle the "Stop" button click event
+    $(".stop-button").click(function() {
+        var nickname = $(this).data("config-nickname");
+        var statusMessageElement = $("#status-message-" + nickname);
+
+        // Change the status message to "Shutting down"
+        statusMessageElement.text("Shutting down");
+
+        // Send a POST request to stop the relay using the corresponding nickname
+        $.post("/relay/stop", { relayNickname: nickname }, function(data) {
+            // Handle the response, e.g., show success or error messages
+        });
+    });
+
     // Periodically update relay status every 10 seconds (adjust the interval as needed)
-    setInterval(updateAllRelayStatus, 1000);
+    setInterval(updateAllRelayStatus, 10000);
 });
