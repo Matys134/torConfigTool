b147d8edbccf0099c4cec6c07e57c32707eff0b4
Matys <koubamates4@gmail.com>
2023-11-02 20:35:23 +0100

    update - edit bridge

--- src/main/java/com/school/torconfigtool/models/BridgeRelayConfig.java
+++ src/main/java/com/school/torconfigtool/models/BridgeRelayConfig.java
@@ -1,35 +1,11 @@
 package com.school.torconfigtool.models;
 
-public class BridgeRelayConfig {
-    private String nickname;
-    private String orPort;
-    private String contact;
-    private String controlPort;
-    private String socksPort;
+public class BridgeRelayConfig extends GuardRelayConfig {
+
     private String bridgeTransportListenAddr;
     private String relayType;
 
     public BridgeRelayConfig() {
-    }
-
-    public void setControlPort(String controlPort) {
-        this.controlPort = controlPort;
-    }
-
-    public void setSocksPort(String socksPort) {
-        this.socksPort = socksPort;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public void setOrPort(String orPort) {
-        this.orPort = orPort;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
     }
 
     public void setBridgeTransportListenAddr(String bridgeTransportListenAddr) {
@@ -40,25 +16,6 @@ public class BridgeRelayConfig {
         this.relayType = relayType;
     }
 
-    public String getNickname() {
-        return nickname;
-    }
-
-    public String getOrPort() {
-        return orPort;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public String getControlPort() {
-        return controlPort;
-    }
-
-    public String getSocksPort() {
-        return socksPort;
-    }
 
     public String getBridgeTransportListenAddr() {
         return bridgeTransportListenAddr;
