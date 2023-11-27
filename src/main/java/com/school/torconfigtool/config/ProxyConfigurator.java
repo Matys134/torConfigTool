94fa0b63376f32b2cae85081ca2b18dd8e66b413
Matys <koubamates4@gmail.com>
2023-11-27 18:29:33 +0100

    Update proxy configurations
    Updated the proxy configuration file (torrc-prox) path and changed some settings. The SocksPort and DNSPort IP addresses were updated to match the new network settings. The DataDirectory line was replaced with the DNSPort setting to enable DNS requests via Tor.

--- src/main/java/com/school/torconfigtool/config/ProxyConfigurator.java
+++ src/main/java/com/school/torconfigtool/config/ProxyConfigurator.java
@@ -7,7 +7,7 @@ public class ProxyConfigurator {
     public static boolean configureProxy() {
         try {
             // Check if the torrc file for the proxy exists, create it if not
-            File torrcFile = new File("torrc/proxy");
+            File torrcFile = new File("torrc/proxy/torrc-proxy");
             if (!torrcFile.exists()) {
                 torrcFile.createNewFile();
             }
@@ -16,13 +16,13 @@ public class ProxyConfigurator {
             FileWriter fw = new FileWriter(torrcFile);
             BufferedWriter bw = new BufferedWriter(fw);
 
-            bw.write("SocksPort 192.168.1.100:9050");
+            bw.write("SocksPort 192.168.2.119:9050");
             bw.newLine();
             bw.write("SocksPolicy accept 192.168.1.0/24");
             bw.newLine();
             bw.write("RunAsDaemon 1");
             bw.newLine();
-            bw.write("DataDirectory /var/lib/tor");
+            bw.write("DNSPort 192.168.2.119:53");
 
             bw.close();
 
