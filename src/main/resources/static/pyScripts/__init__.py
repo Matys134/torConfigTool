f2eef45fd124b0e071f083b7a03e7dd11b865ba9
Matys <koubamates4@gmail.com>
2023-12-04 22:03:02 +0100

    Modify thread target to monitor traffic and flags simultaneously
    This update revises the thread target function in the 'pyScripts' '__init__.py' file to 'monitor_traffic_and_flags'. Rather than monitoring traffic alone, the thread now monitors both the traffic as well as the flags, providing a more comprehensive view of the relay status. The 'relay_flags' function was also added to get the relay's status entry effectively. This should enhance relay monitoring capabilities and efficiency.

--- src/main/resources/static/pyScripts/__init__.py
+++ src/main/resources/static/pyScripts/__init__.py
@@ -25,7 +25,7 @@ def main():
     # Create a thread for each relay and start monitoring
     threads = []
     for control_port in control_ports:
-        thread = threading.Thread(target=monitor_traffic, args=(control_port,))
+        thread = threading.Thread(target=monitor_traffic_and_flags, args=(control_port,))
         thread.setDaemon(True)  # Set the thread as a daemon
         threads.append(thread)
         thread.start()
@@ -48,7 +48,7 @@ def read_control_port(file_path):
         print(f"Error reading control port from {file_path}: {str(e)}")
     return None
 
-def monitor_traffic(control_port):
+def monitor_traffic_and_flags(control_port):
     while True:
         try:
             with Controller.from_port(port=control_port) as controller:
@@ -59,6 +59,9 @@ def monitor_traffic(control_port):
                 print(f"Monitoring relay on ControlPort {control_port}")
 
                 while controller.is_alive():  # Check if the relay is still running
+                    # Get the relay's status entry
+                    flags = relay_flags(controller)
+                    print(f"Relay flags: {flags}")
                     time.sleep(1)  # Wait for 1 second to collect data
 
                 print(f"Relay on ControlPort {control_port} has stopped.")
@@ -73,7 +76,10 @@ def monitor_traffic(control_port):
             # Sleep for a while before retrying
             time.sleep(5)  # Sleep for 5 seconds before retrying
 
-
+def relay_flags(controller):
+    my_fingerprint = controller.get_info("fingerprint")  # Get the relay's fingerprint
+    status = controller.get_network_status(default=my_fingerprint)  # Get the status entry for this relay
+    return status.flags  # Access the flags
 
 
 def _handle_bandwidth_event(controller, control_port, event):
