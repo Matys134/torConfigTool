7066fedc9d38e05a122cfc7f4e9e2ad62b860041
Matys <koubamates4@gmail.com>
2023-12-10 10:55:38 +0100

    update

--- src/main/java/com/school/torconfigtool/controllers/BridgeController.java
+++ src/main/java/com/school/torconfigtool/controllers/BridgeController.java
@@ -79,7 +79,7 @@ public class BridgeController {
             config.setBridgeType(bridgeType);
             config.setWebtunnelDomain(webtunnelDomain);
             config.setWebtunnelUrl(webtunnelUrl);
-            config.setWebtunnelPort(webtunnelPort == null ? 0 : webtunnelPort.intValue());
+            config.setWebtunnelPort(webtunnelPort == null ? 0 : bridgePort.intValue());
             config.setEmail(bridgeContact); // Assume bridgeContact is email here
             if (!torrcFilePath.toFile().exists()) {
                 TorrcFileCreator.createTorrcFile(torrcFilePath.toString(), config);
