c199386ee530e2468d69b04827700399470be9a9
Matys <koubamates4@gmail.com>
2023-12-19 17:20:45 +0100

    update - logs

--- src/main/resources/static/pyScripts/__init__.py
+++ src/main/resources/static/pyScripts/__init__.py
@@ -53,10 +53,18 @@ def monitor_traffic_and_flags(control_port):
         try:
             with Controller.from_port(port=control_port) as controller:
                 controller.authenticate()
+
+                # Enable all events
+                controller.set_conf('__LeaveStreamsUnattached', '1')  # Avoid attaching streams to circuits ourselves
+                controller.set_conf('ControlPort', str(control_port))
+                controller.set_conf('HashedControlPassword', '')
+                controller.set_conf('CookieAuthentication', '1')
+
                 bw_event_handler = functools.partial(_handle_bandwidth_event, controller, control_port)
                 controller.add_event_listener(bw_event_handler, EventType.BW)
 
-                controller.add_event_listener(lambda event: _handle_event(controller, control_port, event))
+                # Add event listener for all events
+                controller.add_event_listener(lambda event: _handle_event(controller, control_port, event), EventType.ALL)
 
                 print(f"Monitoring relay on ControlPort {control_port}")
 
