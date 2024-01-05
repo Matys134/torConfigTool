41e32d6a3b0bbda586d95dfa7f46a8fba9cc8d45
Matys <koubamates4@gmail.com>
2024-01-05 22:56:36 +0100

    changed onion folders and how they are created

--- shellScripts/remove_onion_files.sh
+++ shellScripts/remove_onion_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Commands that require root permissions
-rm -f /etc/nginx/sites-available/onion-service-$1
-rm -f /etc/nginx/sites-enabled/onion-service-$1
+sudo rm -f /etc/nginx/sites-available/onion-service-$1
+sudo rm -f /etc/nginx/sites-enabled/onion-service-$1
