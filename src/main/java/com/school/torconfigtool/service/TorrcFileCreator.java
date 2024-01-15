7bb366681c19dd9a335f43c8494118fa6f269c57
Matys <koubamates4@gmail.com>
2024-01-15 20:20:20 +0100

    changing setup

--- src/main/java/com/school/torconfigtool/service/TorrcFileCreator.java
+++ src/main/java/com/school/torconfigtool/service/TorrcFileCreator.java
@@ -26,8 +26,10 @@ public class TorrcFileCreator {
             writer.newLine();
             writer.write("ORPort " + config.getOrPort());
             writer.newLine();
-            writer.write("ContactInfo " + config.getContact());
-            writer.newLine();
+            if (config.getContact() != null) {
+                writer.write("ContactInfo " + config.getContact());
+                writer.newLine();
+            }
             writer.write("ControlPort " + config.getControlPort());
             writer.newLine();
             writer.write("SocksPort 0");
@@ -47,40 +49,8 @@ public class TorrcFileCreator {
             } else {
                 logger.error("Unknown relay type");
             }
-
-            // Add any other common configurations from BaseRelayConfig
-            String currentDirectory = System.getProperty("user.dir");
-            String dataDirectoryPath = currentDirectory + File.separator + "torrc" + File.separator + "dataDirectory" + File.separator + config.getNickname();
-            writer.write("DataDirectory " + dataDirectoryPath);
-
         } catch (IOException e) {
             logger.error("Error creating Torrc file", e);
         }
-    }
-
-    static String getSystemIpv6() {
-        try {
-            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-            while (interfaces.hasMoreElements()) {
-                NetworkInterface iface = interfaces.nextElement();
-                if (iface.isLoopback() || !iface.isUp())
-                    continue;
-
-                Enumeration<InetAddress> addresses = iface.getInetAddresses();
-                while (addresses.hasMoreElements()) {
-                    InetAddress addr = addresses.nextElement();
-                    if (addr instanceof Inet6Address) {
-                        if (!addr.isLinkLocalAddress()) {
-                            String hostAddress = addr.getHostAddress();
-                            int idx = hostAddress.indexOf('%');
-                            return (idx > 0) ? hostAddress.substring(0, idx) : hostAddress;
-                        }
-                    }
-                }
-            }
-        } catch (SocketException e) {
-            logger.error("Socket exception when getting IPv6 address", e);
-        }
-        return null;
     }
 }
