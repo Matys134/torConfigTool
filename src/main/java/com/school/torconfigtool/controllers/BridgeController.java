478ea029085112c12d9ba253d916ee96e079882e
Matys <koubamates4@gmail.com>
2024-02-03 11:16:48 +0100

    changing setu

--- src/main/java/com/school/torconfigtool/controllers/BridgeController.java
+++ src/main/java/com/school/torconfigtool/controllers/BridgeController.java
@@ -136,7 +136,7 @@ public class BridgeController {
         String certDirectory = programLocation + "/onion/certs/service-80/";
         new File(certDirectory).mkdirs();
 
-        String command = "/home/matys/.acme.sh/acme.sh --issue -d " + webTunnelUrl + " -w " + programLocation + "/onion/www/service-80/ --nginx --server letsencrypt";
+        String command = "/home/matys/.acme.sh/acme.sh --issue -d " + webTunnelUrl + " -w " + programLocation + "/onion/www/service-80/ --nginx --server letsencrypt_test";
         System.out.println("Generating certificate: " + command);
 
         executeCommand(command);
