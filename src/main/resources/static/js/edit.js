6dc947973ddedd2e55c25d7eafcca6ae37538efe
Matys <koubamates4@gmail.com>
2024-02-04 16:12:00 +0100

    changing setu

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -26,24 +26,41 @@ $(document).ready(function () {
         configSelectors.contact.val(data.contact);
         configSelectors.controlPort.val(data.controlPort);
 
-        // Hide all fields initially
-        configSelectors.nickname.closest('label').hide();
-        configSelectors.orPort.closest('label').hide();
-        configSelectors.serverTransport.closest('label').hide();
-        configSelectors.contact.closest('label').hide();
-        configSelectors.controlPort.closest('label').hide();
+        // Show or hide the input fields based on whether the corresponding data attribute has a value
+        configSelectors.nickname.closest('label').toggle(!!data.nickname);
+        configSelectors.orPort.closest('label').toggle(!!data.orPort);
+        configSelectors.contact.closest('label').toggle(!!data.contact);
+        configSelectors.controlPort.closest('label').toggle(!!data.controlPort);
 
-        // Show or hide the input fields based on the relay type
-        if (relayType === 'obfs4') {
+        // Show or hide the serverTransport field based on the relay type
+        if (relayType === 'bridge') {
+            configSelectors.serverTransport.closest('label').show();
+        } else {
+            configSelectors.serverTransport.closest('label').hide();
+        }
+
+        // Show or hide fields based on the type of bridge
+        if (data.bridgeType === 'obfs4') {
             configSelectors.orPort.closest('label').show();
             configSelectors.serverTransport.closest('label').show();
             configSelectors.contact.closest('label').show();
             configSelectors.controlPort.closest('label').show();
-        } else if (relayType === 'webtunnel') {
+        } else if (data.bridgeType === 'WebTunnel') {
+            configSelectors.orPort.closest('label').hide();
+            configSelectors.serverTransport.closest('label').hide();
             configSelectors.contact.closest('label').show();
-        } else if (relayType === 'snowflake') {
-            // No fields to show for snowflake
+            configSelectors.controlPort.closest('label').hide();
+        } else if (data.bridgeType === 'Snowflake') {
+            configSelectors.orPort.closest('label').hide();
+            configSelectors.serverTransport.closest('label').hide();
+            configSelectors.contact.closest('label').hide();
+            configSelectors.controlPort.closest('label').hide();
         }
+
+        // Set the data-config-type attribute of each field to the relay type
+        $('#edit-form [data-config-type]').each(function() {
+            $(this).toggle($(this).attr('data-config-type').split(' ').includes(relayType));
+        });
 
         // Show the modal
         $('#edit-modal').modal('show');
