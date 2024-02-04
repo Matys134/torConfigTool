cc295695e8171936ae7691e89e67a852f6c26e72
Matys <koubamates4@gmail.com>
2024-02-04 11:02:59 +0100

    changing setu

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -26,23 +26,33 @@ $(document).ready(function () {
         configSelectors.contact.val(data.contact);
         configSelectors.controlPort.val(data.controlPort);
 
-        // Show or hide the input fields based on whether the corresponding data attribute has a value
-        configSelectors.nickname.closest('label').toggle(!!data.nickname);
-        configSelectors.orPort.closest('label').toggle(!!data.orPort);
-        configSelectors.contact.closest('label').toggle(!!data.contact);
-        configSelectors.controlPort.closest('label').toggle(!!data.controlPort);
-
-        // Show or hide the serverTransport field based on the relay type
-        if (relayType === 'bridge') {
-            configSelectors.serverTransport.closest('label').show();
-        } else {
-            configSelectors.serverTransport.closest('label').hide();
+        // Show or hide the input fields based on the relay type
+        switch (relayType) {
+            case 'guard':
+                configSelectors.orPort.closest('label').show();
+                configSelectors.contact.closest('label').show();
+                configSelectors.controlPort.closest('label').show();
+                configSelectors.serverTransport.closest('label').hide();
+                break;
+            case 'bridge':
+                configSelectors.orPort.closest('label').show();
+                configSelectors.serverTransport.closest('label').show();
+                configSelectors.contact.closest('label').show();
+                configSelectors.controlPort.closest('label').show();
+                break;
+            case 'webtunnel':
+                configSelectors.contact.closest('label').show();
+                configSelectors.orPort.closest('label').hide();
+                configSelectors.serverTransport.closest('label').hide();
+                configSelectors.controlPort.closest('label').hide();
+                break;
+            case 'onion':
+                configSelectors.orPort.closest('label').hide();
+                configSelectors.serverTransport.closest('label').hide();
+                configSelectors.contact.closest('label').hide();
+                configSelectors.controlPort.closest('label').hide();
+                break;
         }
-
-        // Set the data-config-type attribute of each field to the relay type
-        $('#edit-form [data-config-type]').each(function() {
-            $(this).toggle($(this).attr('data-config-type').split(' ').includes(relayType));
-        });
 
         // Show the modal
         $('#edit-modal').modal('show');
