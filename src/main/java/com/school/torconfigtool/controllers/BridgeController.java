a546fb598e2bdd0bdc95362affec062906166c0e
Matys <koubamates4@gmail.com>
2024-02-06 00:06:32 +0100

    Zabiju Vojtěcha

--- src/main/java/com/school/torconfigtool/controllers/BridgeController.java
+++ src/main/java/com/school/torconfigtool/controllers/BridgeController.java
@@ -154,8 +154,8 @@ public class BridgeController {
         String certDirectory = programLocation + "/onion/certs/service-80/";
         new File(certDirectory).mkdirs();
 
-        String command = "/home/matys/.acme.sh/acme.sh --issue -d " + webTunnelUrl + " -w " + programLocation + "/onion/www/service-80/ --nginx --server letsencrypt_test --force";
-        System.out.println("Generating certificate: " + command + " --force");
+        String command = "/home/matys/.acme.sh/acme.sh --issue -d " + webTunnelUrl + " -w " + programLocation + "/onion/www/service-80/ --nginx --server letsencrypt --force";
+        System.out.println("Generating certificate: " + command);
 
         Process certProcess = executeCommand(command);
         if (certProcess == null || certProcess.exitValue() != 0) {
