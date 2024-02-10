a4698d4481ea3bfadbb3b7d4188e5f898ba2797e
Matys <koubamates4@gmail.com>
2024-02-10 20:46:06 +0100

    Refactoring IpAddressRetriever

--- src/main/java/com/school/torconfigtool/config/IpAddressRetriever.java
+++ src/main/java/com/school/torconfigtool/config/IpAddressRetriever.java
@@ -9,18 +9,32 @@ import java.net.SocketException;
 import java.util.Collections;
 import java.util.Optional;
 
+/**
+ * This class is used to retrieve the local IP address of the machine where the application is running.
+ */
 public class IpAddressRetriever {
 
+    // Logger instance for logging events
     private static final Logger LOGGER = LoggerFactory.getLogger(IpAddressRetriever.class);
 
+    /**
+     * This method is used to get the local IP address.
+     * @return An Optional<String> containing the local IP address if found, otherwise an empty Optional.
+     */
     public Optional<String> getLocalIpAddress() {
         return getFirstNonLoopbackAddress();
     }
 
+    /**
+     * This method is used to get the first non-loopback address from the network interfaces.
+     * @return An Optional<String> containing the first non-loopback address if found, otherwise an empty Optional.
+     */
     private Optional<String> getFirstNonLoopbackAddress() {
         try {
             return Collections.list(NetworkInterface.getNetworkInterfaces()).stream()
-                    .flatMap(networkInterface -> getFirstNonLoopbackAddressFromInterface(networkInterface).stream())
+                    .map(this::getFirstNonLoopbackAddressFromInterface)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
                     .findFirst();
         } catch (SocketException e) {
             LOGGER.error("Failed to get local IP address", e);
@@ -28,6 +42,11 @@ public class IpAddressRetriever {
         }
     }
 
+    /**
+     * This method is used to get the first non-loopback address from a specific network interface.
+     * @param networkInterface The network interface to check for non-loopback addresses.
+     * @return An Optional<String> containing the first non-loopback address if found, otherwise an empty Optional.
+     */
     private Optional<String> getFirstNonLoopbackAddressFromInterface(NetworkInterface networkInterface) {
         return Collections.list(networkInterface.getInetAddresses()).stream()
                 .filter(this::isNonLoopbackSiteLocalAddress)
@@ -35,6 +54,11 @@ public class IpAddressRetriever {
                 .findFirst();
     }
 
+    /**
+     * This method is used to check if an InetAddress is a non-loopback and site local address.
+     * @param inetAddress The InetAddress to check.
+     * @return A boolean indicating whether the InetAddress is a non-loopback and site local address.
+     */
     private boolean isNonLoopbackSiteLocalAddress(InetAddress inetAddress) {
         return !inetAddress.isLoopbackAddress() && inetAddress.isSiteLocalAddress();
     }
