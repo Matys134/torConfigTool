6456bc7dccaeed27c706f330b86066ba6465cde1
Matys <koubamates4@gmail.com>
2024-02-14 16:49:59 +0100

    Revert "Refactoring BridgeRelayConfig - 2"
    This reverts commit ea92d9dc7632eb19cc4b617a136d1e4ae67b2557.

--- src/main/java/com/school/torconfigtool/BridgeRelayConfig.java
+++ src/main/java/com/school/torconfigtool/BridgeRelayConfig.java
@@ -8,8 +8,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -19,7 +17,7 @@ public class BridgeRelayConfig extends BaseRelayConfig {
     private String webtunnelDomain;
     private int webtunnelPort;
     private String webtunnelUrl;
-    private String contact;
+    private String email;
     private String path;
     private String bridgeType;
     private static final Logger logger = LoggerFactory.getLogger(BridgeRelayConfig.class);
@@ -27,23 +25,49 @@ public class BridgeRelayConfig extends BaseRelayConfig {
 
     private SnowflakeProxyRunner snowflakeProxyRunner = new SnowflakeProxyRunner();
 
-    private Map<String, BridgeConfigWriter> configWriters;
-
-    public BridgeRelayConfig() {
-        configWriters = new HashMap<>();
-        configWriters.put("obfs4", new Obfs4ConfigWriter(serverTransport, contact));
-        configWriters.put("webtunnel", new WebtunnelConfigWriter(webtunnelUrl, path));
-        configWriters.put("snowflake", new SnowflakeConfigWriter(snowflakeProxyRunner));
-    }
-
     @Override
     public void writeSpecificConfig(BufferedWriter writer) throws IOException {
-        BridgeConfigWriter configWriter = configWriters.get(getBridgeType());
-        if (configWriter != null) {
-            configWriter.writeConfig(writer);
-        } else {
-            logger.error("Unknown bridge type: " + getBridgeType());
+        writer.write("BridgeRelay 1");
+        writer.newLine();
+        logger.info("Writing specific config for bridge type: " + getBridgeType());
+        switch (getBridgeType()) {
+            case "obfs4":
+                writeObfs4Config(writer);
+                break;
+            case "webtunnel":
+                writeWebtunnelConfig(writer);
+                break;
+            case "snowflake":
+                writeSnowflakeConfig();
+                break;
+            default:
+                logger.error("Unknown bridge type: " + getBridgeType());
         }
+    }
+
+    private void writeConfig(BufferedWriter writer, String bridgeType, String execCommand, String listenAddress, String additionalOptions) throws IOException {
+        writer.write("ServerTransportPlugin " + bridgeType + " exec " + execCommand);
+        writer.newLine();
+        writer.write("ServerTransportListenAddr " + bridgeType + " " + listenAddress);
+        writer.newLine();
+        writer.write("ExtORPort auto");
+        writer.newLine();
+        if (additionalOptions != null) {
+            writer.write(additionalOptions);
+            writer.newLine();
+        }
+    }
+
+    private void writeObfs4Config(BufferedWriter writer) throws IOException {
+        writeConfig(writer, "obfs4", "/usr/bin/obfs4proxy", "0.0.0.0:" + getServerTransport(), "ContactInfo " + getContact());
+    }
+
+    private void writeWebtunnelConfig(BufferedWriter writer) throws IOException {
+        writeConfig(writer, "webtunnel", "/usr/local/bin/webtunnel", "127.0.0.1:15000", "ServerTransportOptions webtunnel url=https://" + getWebtunnelUrl() + "/" + getPath());
+    }
+
+    private void writeSnowflakeConfig() {
+        snowflakeProxyRunner.runSnowflakeProxy();
     }
 
     public void setBridgeType(String bridgeType) {
