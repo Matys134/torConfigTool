38298f904ac07e59a88cf06574050c9a0f180e5c
Matys <koubamates4@gmail.com>
2024-02-15 18:48:19 +0100

    refactoring

--- src/main/java/com/school/torconfigtool/nginx/service/NginxConfigGenerator.java
+++ src/main/java/com/school/torconfigtool/nginx/service/NginxConfigGenerator.java
@@ -1,6 +1,5 @@
 package com.school.torconfigtool.nginx.service;
 
-import com.school.torconfigtool.NginxFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
