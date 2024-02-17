f0e7f8497cc68fb22b017c01d84fa2d9b06a50d5
Matys <koubamates4@gmail.com>
2024-02-17 21:29:34 +0100

    Revert "Merge pull request #159 from Matys134/refactoring"
    This reverts commit 922116b4bc3cf3b55c6afaa7c1ebbd180da907c6, reversing
    changes made to ad71b654976594f7ebc2e801b8a752cb2e3c5a88.

--- src/main/java/com/school/torconfigtool/CommandExecutor.java
+++ src/main/java/com/school/torconfigtool/CommandExecutor.java
@@ -6,42 +6,23 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-/**
- * This class is responsible for executing shell commands.
- * It is annotated with @Service to indicate that it's a service component in Spring framework.
- */
 @Service
 public class CommandExecutor {
-    // Logger instance for this class
     private static final Logger logger = LoggerFactory.getLogger(CommandExecutor.class);
 
-    /**
-     * Executes a shell command and returns the resulting process.
-     * If the command execution fails, it logs the error and throws an IOException.
-     *
-     * @param command The shell command to execute
-     * @return The resulting Process after command execution
-     * @throws IOException If an I/O error occurs
-     * @throws InterruptedException If the current thread is interrupted while waiting for the process to end
-     */
-    public Process executeCommand(String command) throws IOException, InterruptedException {
-        // Create a new process builder
+    public Process executeCommand(String command) {
         ProcessBuilder processBuilder = new ProcessBuilder();
-
-        // Set the command for the process builder to execute
         processBuilder.command("bash", "-c", command);
-
-        // Start the process and wait for it to end
-        Process process = processBuilder.start();
-        int exitCode = process.waitFor();
-
-        // If the exit code is not 0, log the error and throw an IOException
-        if (exitCode != 0) {
-            logger.error("Error during command execution. Command: {}, Exit code: {}", command, exitCode);
-            throw new IOException("Error during command execution. Exit code: " + exitCode);
+        try {
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                logger.error("Error during command execution. Exit code: " + exitCode);
+            }
+            return process;
+        } catch (IOException | InterruptedException e) {
+            logger.error("Error during command execution", e);
+            return null;
         }
-
-        // Return the resulting process
-        return process;
     }
 }
