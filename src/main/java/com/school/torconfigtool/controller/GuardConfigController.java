b2fcd38474208f91baf5f8c7b0170464eeb8fc8e
Matys <koubamates4@gmail.com>
2024-02-22 22:10:52 +0100

    editing status error fixing

--- src/main/java/com/school/torconfigtool/controller/GuardConfigController.java
+++ src/main/java/com/school/torconfigtool/controller/GuardConfigController.java
@@ -1,18 +1,20 @@
 package com.school.torconfigtool.controller;
 
-import com.school.torconfigtool.model.GuardConfig;
 import com.school.torconfigtool.service.GuardConfigService;
+import com.school.torconfigtool.model.GuardConfig;
+import com.school.torconfigtool.util.RelayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+/**
+ * GuardConfigController is a Spring MVC RestController that handles HTTP requests related to updating the GuardConfig.
+ * It uses GuardConfigService to perform the actual operations.
+ */
 @RestController
 @RequestMapping("/update-guard-config")
 public class GuardConfigController {
@@ -20,10 +22,21 @@ public class GuardConfigController {
     private final GuardConfigService guardConfigService;
     private static final Logger logger = LoggerFactory.getLogger(GuardConfigController.class);
 
+    /**
+     * Constructs a new GuardConfigController with the provided GuardConfigService.
+     *
+     * @param guardConfigService the GuardConfigService to be used for GuardConfig operations
+     */
     public GuardConfigController(GuardConfigService guardConfigService) {
         this.guardConfigService = guardConfigService;
     }
 
+    /**
+     * Handles POST requests to update the GuardConfig.
+     *
+     * @param config the GuardConfig to be updated
+     * @return a ResponseEntity that contains the result of the update operation
+     */
     @PostMapping
     public ResponseEntity<Map<String, String>> updateGuardConfiguration(@RequestBody GuardConfig config) {
         logger.info("Received request to update guard configuration: {}", config);
@@ -34,5 +47,12 @@ public class GuardConfigController {
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
+    }
+
+    @GetMapping("/check-port-availability")
+    public ResponseEntity<?> checkPortAvailability(@RequestParam String nickname, @RequestParam int orPort, @RequestParam int controlPort) {
+        boolean arePortsAvailable = RelayUtils.portsAreAvailable(nickname, orPort, controlPort);
+
+        return ResponseEntity.ok(Map.of("available", arePortsAvailable));
     }
 }
