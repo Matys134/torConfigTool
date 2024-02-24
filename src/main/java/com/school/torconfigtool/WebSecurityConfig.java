997e0a2fefa001d92b16486eb6f364c478f8a474
Matys <koubamates4@gmail.com>
2024-02-24 20:51:51 +0100

    Update

--- src/main/java/com/school/torconfigtool/WebSecurityConfig.java
+++ src/main/java/com/school/torconfigtool/WebSecurityConfig.java
@@ -1,13 +1,6 @@
 package com.school.torconfigtool;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 /*
 @Configuration
 @EnableWebSecurity*/
