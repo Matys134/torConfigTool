63c2a32f39c59ca899a995a35ad833f0ec5dfe13
Matys <koubamates4@gmail.com>
2024-02-27 23:50:33 +0100

    update - fixing control ports fetching

--- src/main/java/com/school/torconfigtool/WebSecurityConfig.java
+++ src/main/java/com/school/torconfigtool/WebSecurityConfig.java
@@ -1,20 +1,5 @@
 package com.school.torconfigtool;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 import java.io.IOException;
 import java.nio.file.Files;
