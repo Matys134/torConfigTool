edcaeeb3be0b772b95f03fa5048d321554aefb57
Matys <koubamates4@gmail.com>
2024-02-28 21:07:48 +0100

    testing if this fixes API endpoint for python script

--- src/main/java/com/school/torconfigtool/WebSecurityConfig.java
+++ src/main/java/com/school/torconfigtool/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/api/*").permitAll()
+                        .requestMatchers("/api/*", "/api/*/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
