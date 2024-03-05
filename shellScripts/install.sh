a6449e9934771ae3ff4b66170dbafd3b38ec0384
Matys <koubamates4@gmail.com>
2024-03-05 21:11:31 +0100

    updated openPort80

--- shellScripts/install.sh
+++ shellScripts/install.sh
@@ -25,7 +25,9 @@ wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8C
 
 # Update the package list and install Tor and the Tor Project keyring
 apt update
-apt install -y tor deb.torproject.org-keyring -t bullseye-backports obfs4proxy nginx snowflake-proxy
+apt install -t bullseye-backports obfs4proxy
+apt install snowflake-proxy
+apt install nginx
 
 # Get the user who launched the script
 current_user=$SUDO_USER
@@ -68,74 +70,8 @@ echo "Installation and configuration completed successfully."
 sudo systemctl disable tor.service
 sudo systemctl disable nginx.service
 
-# Define the sudoers entry for the specified user to control Tor without a password
-sudoers_tor_entry="$current_user ALL=(ALL) NOPASSWD: /bin/systemctl start tor, /bin/systemctl stop tor, /bin/systemctl restart tor"
-
-# Define the sudoers entry for the specified user to launch tor using tor -f without a password
-sudoers_tor_entry="$current_user ALL=(ALL) NOPASSWD: /usr/bin/tor -f /etc/tor/torrc"
-
-# Define the sudoers entry for the specified user to edit the nginx config and restart nginx without a password
-sudoers_edit_nginx_entry="$current_user ALL=ALL) NOPASSWD: /bin/cp /etc/nginx/sites-available/default /etc/nginx/sites-available/*.conf, /bin/systemctl restart nginx"
-
-# Define the sudoers entry for the specified user to reload Nginx without a password
-sudoers_nginx_reload_entry="$current_user ALL=(ALL) NOPASSWD: /bin/systemctl reload nginx"
-
-sudoers_nginx_start_entry="$current_user ALL=(ALL) NOPASSWD: /bin/systemctl start nginx"
-
-sudoers_nginx_stop_entry="$current_user ALL=(ALL) NOPASSWD: /bin/systemctl stop nginx"
-
-# Define the sudoers entry for the specified user to control snowflake-proxy without a password
-sudoers_snowflake_proxy_entry="$current_user ALL=(ALL) NOPASSWD: /bin/systemctl start snowflake-proxy, /bin/systemctl stop snowflake-proxy"
-
-sudoers_chown_entry="$current_user ALL=(ALL) NOPASSWD: /bin/chown -R"
-
-# Define the sudoers entry for the specified user to use kill without a password
-sudoers_kill_entry="$current_user ALL=(ALL) NOPASSWD: /bin/kill"
-
-# Define the sudoers entry for the specified user to use ln -s without a password
-sudoers_ln_entry="$current_user ALL=(ALL) NOPASSWD: /bin/ln -s"
-
-# Define the path to the sudoers file
-sudoers_file="/etc/sudoers.d/tor_nginx_sudoers"
-
-# Define the sudoers entry for the specified user to run rm without a password
-sudoers_rm_entry="$current_user ALL=(ALL) NOPASSWD: /bin/rm"
-
-# Check if the sudoers file exists, and create it if it doesn't
-if [ ! -f "$sudoers_file" ]; then
-    touch "$sudoers_file"
-fi
-
-# Define the sudoers entry for the specified user to control Nginx without a password
-sudoers_nginx_entry="$current_user ALL=(ALL) NOPASSWD: /bin/systemctl start nginx, /bin/systemctl stop nginx, /bin/systemctl restart nginx"
-
-# Check if the sudoers file is writable
-if [ -w "$sudoers_file" ]; then
-    # Clear the sudoers file
-    > "$sudoers_file"
-
-    # Add the sudoers entries
-    {
-        echo "$sudoers_tor_entry"
-        echo "$sudoers_edit_nginx_entry"
-        echo "$sudoers_nginx_entry"
-        echo "$sudoers_snowflake_proxy_entry"
-        echo "$sudoers_chown_entry"
-        echo "$sudoers_nginx_reload_entry"
-        echo "$sudoers_kill_entry"
-        echo "$sudoers_nginx_start_entry"
-        echo "$sudoers_nginx_stop_entry"
-        echo "$sudoers_ln_entry"
-        echo "$sudoers_rm_entry"
-    } | sudo tee "$sudoers_file" > /dev/null
-
-    echo "Sudoers entries added for $current_user to control Tor, Nginx, copy Nginx config files, edit Nginx config files, and restart Nginx without a password."
-else
-    echo "Cannot write to $sudoers_file. Please run this script with superuser privileges."
-fi
-
 # Change the ownership of the nginx default site configuration file
-sudo chown $SUDO_USER /etc/nginx/sites-available/default
+# sudo chown $SUDO_USER /etc/nginx/sites-available/default
 
 # Check if email argument is provided
 if [ -z "$1" ]
