c709b96de8825728d994997aff9ab651cc5a37ee
kouba <koubamates4@gmail.com>
2024-03-14 19:18:10 +0100

    refactoring

--- src/main/java/com/school/torconfigtool/controller/SetupRestController.java
+++ src/main/java/com/school/torconfigtool/controller/SetupRestController.java
@@ -1,7 +1,6 @@
 package com.school.torconfigtool.controller;
 
 import com.school.torconfigtool.service.RelayInformationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
