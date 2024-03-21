43c82e5b8bb2b02747fb2d85d5e88a74ad39fa7c
kouba <koubamates4@gmail.com>
2024-03-21 17:45:01 +0100

    update

--- data.py
+++ data.py
@@ -124,7 +124,7 @@ def _send_relay_data_entry(control_port, relay_data_entry):
     api_endpoint = f"{BASE_API_ENDPOINT}/{control_port}"
 
     # Send the relay data entry to the API endpoint for the corresponding relay
-    response = requests.post(api_endpoint, json=relay_data_entry)
+    response = requests.post(api_endpoint, json=relay_data_entry, verify=False)
 
     if response.status_code == 200:
         print(f"Data sent for ControlPort {control_port}: {relay_data_entry}")
