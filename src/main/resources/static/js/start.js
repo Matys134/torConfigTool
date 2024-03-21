130cf9de158f814a6fdf4c26082a335177822c1e
kouba <koubamates4@gmail.com>
2024-03-21 19:03:48 +0100

    update

--- src/main/resources/static/js/start.js
+++ src/main/resources/static/js/start.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-    // Add a click event handler for the "Start" buttons
     $(".start-button").click(function () {
         const nickname = $(this).data("config-nickname");
         const relayType = $(this).data("config-type");
+
+        // Select the start, stop and edit buttons
+        const startButton = $(".start-button[data-config-nickname='" + nickname + "']");
+        const stopButton = $(".stop-button[data-config-nickname='" + nickname + "']");
+        const editButton = $(".edit-button[data-config-nickname='" + nickname + "']");
+
+        // Disable the buttons
+        startButton.prop('disabled', true);
+        stopButton.prop('disabled', true);
+        editButton.prop('disabled', true);
 
         // Show the appropriate spinner
         $("#spinner-" + relayType).show();
@@ -24,6 +33,12 @@ $(document).ready(function () {
                 // Hide the spinner
                 $("#spinner").hide();
                 console.error('Error starting relay:', error);
+            },
+            complete: function () {
+                // Enable the buttons
+                startButton.prop('disabled', false);
+                stopButton.prop('disabled', false);
+                editButton.prop('disabled', false);
             }
         });
     });
