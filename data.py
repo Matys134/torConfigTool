3560995e25c07d3986ed7a443f668b853102fa50
kouba <koubamates4@gmail.com>
2024-03-21 17:43:32 +0100

    update

--- data.py
+++ data.py
@@ -194,7 +194,7 @@ def _send_bandwidth_data(controller, control_port):
     api_endpoint = f"{BASE_API_ENDPOINT}/{control_port}"
 
     # Send data to the API endpoint for the corresponding relay
-    response = requests.post(api_endpoint, json=data, verify=False)
+    response = requests.post(api_endpoint, json=data)
 
     # Print the response object
     print(f"Response for ControlPort {control_port}: {response}")
