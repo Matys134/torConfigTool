676ab0edddf5cb6c73b43a431e879e52938622f1
kouba <koubamates4@gmail.com>
2024-03-21 18:36:08 +0100

    update

--- src/main/resources/static/js/start.js
+++ src/main/resources/static/js/start.js
@@ -1,25 +1,31 @@
-$(document).ready(function () {
-    // Add a click event handler for the "Start" buttons
-    $(".start-button").click(function () {
-        const nickname = $(this).data("config-nickname");
-        const relayType = $(this).data("config-type");
+$(".start-button").click(function () {
+    const nickname = $(this).data("config-nickname");
+    const relayType = $(this).data("config-type");
 
-        // Send an AJAX request to start the relay
-        $.ajax({
-            url: '/relay-operations-api/start',
-            type: 'POST',
-            data: {
-                relayNickname: nickname,
-                relayType: relayType
-            },
-            success: function (data) {
-                // Handle the server response here (if needed)
-                console.log('Relay started:', data);
-            },
-            error: function (error) {
-                // Handle any errors here
-                console.error('Error starting relay:', error);
-            }
-        });
+    // Show the spinner
+    $("#spinner-" + nickname).show();
+
+    // Send an AJAX request to start the relay
+    $.ajax({
+        url: '/relay-operations-api/start',
+        type: 'POST',
+        data: {
+            relayNickname: nickname,
+            relayType: relayType
+        },
+        success: function (data) {
+            // Handle the server response here (if needed)
+            console.log('Relay started:', data);
+
+            // Hide the spinner
+            $("#spinner-" + nickname).hide();
+        },
+        error: function (error) {
+            // Handle any errors here
+            console.error('Error starting relay:', error);
+
+            // Hide the spinner
+            $("#spinner-" + nickname).hide();
+        }
     });
 });
